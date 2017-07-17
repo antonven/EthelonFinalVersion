93cacf80a29469240e47a2d09c584733586dea13
kobe <kobearelativo@gmail.com>
2017-07-17 23:11:40 +0800

    firebase Connect

--- app/src/main/java/myapps/wycoco/com/ethelonfinalversion/Activities/LoginActivity.java
+++ app/src/main/java/myapps/wycoco/com/ethelonfinalversion/Activities/LoginActivity.java
@@ -9,10 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import myapps.wycoco.com.ethelonfinalversion.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Firebase firebase;
     TextView signUp;
     EditText inputEmail, inputPassword;
     Button buttonLogin, buttonFacebook, buttonGoogle ;
@@ -21,6 +26,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Firebase.setAndroidContext(this);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference table = database.getReference("sampleTable");
+
+         firebase = new Firebase("https://ethelonfinalversion.firebaseio.com/sampleTable");
+
+        firebase.child("name").setValue("Kyle");
+
+
+
+
+
 
 
         signUp = (TextView)findViewById(R.id.buttonSignup);
@@ -36,6 +54,7 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                firebase.child("kobe").setValue("Kyle");
                 startActivity(new Intent(LoginActivity.this,InterestsActivity.class));
             }
         });
